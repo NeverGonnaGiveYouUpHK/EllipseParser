1c6acfd583c60056dc57b910b8c21fbaa43a19dc
GLise710 <matejkozub124@gmail.com>
2021-05-07 15:04:53 +0200

    some progress, paths done

--- EllipseParser/Main.cpp
+++ EllipseParser/Main.cpp
@@ -4,8 +4,58 @@
 
 #include <math.h>
 
+#include <Eigen/Dense>
+using namespace Eigen;
+
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+
+struct Point {
+	uint32 x;
+	uint32 y;
+};
+
+struct ellipseABCDEF {
+	double A;
+	double B;
+	double C;
+	double D;
+	double E;
+	double F;
+};
+
+struct ellipseABCDEF fitEllipse(struct Point p1, struct Point p2, struct Point p3, struct Point p4, struct Point p5) {
+	Matrix<double, 5, 6> mat56(5, 6);
+	mat56 << 
+		p1.x * p1.x, p1.y * p1.y, p1.x * p1.y, p1.x, p1.y, 1,
+		p2.x * p2.x, p2.y * p2.y, p2.x * p2.y, p2.x, p2.y, 1,
+		p3.x * p3.x, p3.y * p3.y, p3.x * p3.y, p3.x, p3.y, 1,
+		p4.x * p4.x, p4.y * p4.y, p4.x * p4.y, p4.x, p4.y, 1,
+		p5.x * p5.x, p5.y * p5.y, p5.x * p5.y, p5.x, p5.y, 1;
+	
+	CompleteOrthogonalDecomposition<Matrix<double, 5, 6> > cod;
+	cod.compute(mat56);
+	std::cout << "rank : " << cod.rank() << "\n";
+	// Find URV^T
+	MatrixXd V = cod.matrixZ().transpose();
+	MatrixXd Null_space = V.block(0, cod.rank(), V.rows(), V.cols() - cod.rank());
+	MatrixXd P = cod.colsPermutation();
+	Null_space = P * Null_space; // Unpermute the columns
+	// The Null space:
+	std::cout << "The null space: \n" << Null_space << "\n";
+	// Check that it is the null-space:
+	std::cout << "mat56 * Null_space = \n" << mat56 * Null_space << '\n';
+
+	return {
+		Null_space(0),
+		Null_space(1),
+		Null_space(2),
+		Null_space(3),
+		Null_space(4),
+		Null_space(5)
+	};
+}
+
 
 static uint16 finalAverage;
 
@@ -34,10 +84,7 @@ public:
 
 typedef unsigned long long uint64;
 
-struct Point {
-	uint32 x;
-	uint32 y;
-};
+
 
 struct followReturn {
 	bool success;
@@ -69,7 +116,7 @@ bool validateEdge(bool* considered, struct Point point, uint32 width, uint32 hei
 
 
 int main(int argc, char** argv) {
-	TIFF* tif = TIFFOpen("./test/2018-02-15 17.55.41.631000.tiff", "r");
+	TIFF* tif = TIFFOpen("./test/2018-02-15 19.22.13.141000.tiff", "r");
 
 	uint32 width, height;
 
@@ -93,7 +140,7 @@ int main(int argc, char** argv) {
 
 	std::cout << average / (uint64(width) * uint64(height)) << std::endl;
 
-	finalAverage = average / (uint64(width) * uint64(height)) * 28;
+	finalAverage = average / (uint64(width) * uint64(height)) * 1;
 
 	std::cout << finalAverage << std::endl;
 
@@ -111,9 +158,13 @@ int main(int argc, char** argv) {
 
 	std::vector<std::vector<struct Point>> sequences;
 
-	sequences.push_back(std::vector<struct Point>());
+	std::vector<struct Point> paths[2] = {
+		std::vector<struct Point>(),
+		std::vector<struct Point>()
+	};
 
-	int pathCount = 0;
+	std::cout << validateEdge(considered, { 393, 341 }, width, height);
+
 
 	for (uint32 y = 0; y < height; y++) {
 		for (uint32 x = 0; x < width; x++) {
@@ -134,6 +185,8 @@ int main(int argc, char** argv) {
 
 			uint32 newX = x, newY = y;
 
+			printf("newX %lu newY %lu\n", newX, newY);
+
 			struct Point nearbyInitial[8] = {
 				{newX - 1, newY - 1},
 				{newX + 0, newY - 1},
@@ -144,9 +197,12 @@ int main(int argc, char** argv) {
 				{newX + 0, newY + 1},
 				{newX + 1, newY + 1}
 			};
-			bool isEmptyFlag = true;
+
 			uint32 part = 0;
 			uint32 firstPointIndex = 0;
+
+			
+
 			while (part < 2 && firstPointIndex < 8) {
 				newX = nearbyInitial[firstPointIndex].x;
 				newY = nearbyInitial[firstPointIndex].y;
@@ -155,9 +211,9 @@ int main(int argc, char** argv) {
 				if (newX == 0xFFFFFFFF || newY == 0xFFFFFFFF || newX == width || newY == height) {
 					continue;
 				}
-
-
-
+				if (!validateEdge(considered, { newX, newY }, width, height)) {
+					continue;
+				}
 
 				bool endPath = false;
 				do {
@@ -191,11 +247,14 @@ int main(int argc, char** argv) {
 								continue;
 							}
 
+
+							paths[part].push_back({ newX, newY });
+
+
+							printf("adding %lu %lu\n", newX, newY);
+
 							newX = tested.x;
 							newY = tested.y;
-
-							printf("%lu %lu\n", newX, newY);
-							isEmptyFlag = false;
 
 							endPath = false;
 
@@ -209,16 +268,55 @@ int main(int argc, char** argv) {
 
 				part++;
 			}
-			//printf("%d paths\n", pathCount);
-			if (!isEmptyFlag) {
-				printf("%d paths\n", pathCount);
-				pathCount++;
+
+			uint32 sizePart1 = paths[0].size();
+			uint32 sizePart2 = paths[1].size();
+
+			if (sizePart1 + sizePart2 >= 10) {
+				std::vector<struct Point> finalPath = std::vector<struct Point>();
+
+				finalPath.reserve(sizePart1 + sizePart2);
+
+				for (int i = sizePart1 - 1; i >= 0; i--) {
+					printf("%lu\n", i);
+					finalPath.push_back(paths[0][i]);
+					
+				}
+
+				int a = 0;
+
+				for (int i = 0; i < sizePart2; i++) {
+					finalPath.push_back(paths[1][i]);
+				}
+
+				paths[0] = std::vector<struct Point>();
+				paths[1] = std::vector<struct Point>();
+		
+				sequences.push_back(finalPath);
+			} else {
+				paths[0].clear();
+				paths[1].clear();
 			}
-			
 		}
 	}
 
-	printf("%d paths\n", pathCount);
+	printf("%d paths\n", sequences.size());
+
+	if (sequences.size() == 0) {
+		//NO ELLIPSE AT ALL
+	}
+	
+	uint32 maxLength = 0;
+	uint32 longestIndex = 0;
+	for (uint32 i = 0; i < sequences.size(); i++) {
+		if (sequences[i].size() > maxLength) {
+			maxLength = sequences[i].size();
+			longestIndex = i;
+		}
+	}
+	
+	std::vector<struct Point> longest = sequences[longestIndex];
+
 
 
 
